3df9f6ab2a7f2faa28f52ae77f11831cfc99daea
deennoo <deennoo33@gmail.com>
2018-11-17 15:45:02 +0100

    add set_brightness_and_color_temperature
    adding set_brightness_and_color_temperature command 
    
    thx olie93

--- MyBulb.py
+++ MyBulb.py
@@ -13,14 +13,25 @@ import miio.philips_bulb
 parser = argparse.ArgumentParser(description='Script which comunicate with PhilipsBulb.')
 parser.add_argument('IPaddress', help='IP address of PhilipsBulb' )
 parser.add_argument('token', help='token to login to device')
-parser.add_argument('--level', type=int, choices=range(0, 100), help='choose level')
-parser.add_argument('--temp', type=int, choices=range(0, 100), help='choose White Temp')
-parser.add_argument('--power', choices=['ON', 'OFF'], help='power ON/OFF')
 parser.add_argument('--debug', action='store_true', help='if define more output is printed')
+
+# --power = on() or off()
+parser.add_argument('--power', choices=['ON', 'OFF'], help='power ON/OFF')
+
+# --level = set_brightness()
+parser.add_argument('--level', type=int, choices=range(0, 100), help='choose level')
+
+# --brightemp = set_brightness_and_color_temperature()
+parser.add_argument('--brightemp', type=str, help='choose Brightness and White Temp')
+
+# --temp = set_color_temperature()
+parser.add_argument('--temp', type=int, choices=range(0, 100), help='choose White Temp')
+
+# --scene = set_scene()
 parser.add_argument('--scene', type=int, choices=range(1, 5), help='choose scene')
 
-
 args = parser.parse_args()
+
 if args.debug:
     print(args)
 MyBulb = miio.philips_bulb.PhilipsBulb(args.IPaddress, args.token)
@@ -30,6 +41,17 @@ if args.level:
     
 if args.temp:
 	MyBulb.set_color_temperature(args.temp)
+	
+if args.brightemp:
+	#print(args.brightemp)
+	brightness = args.brightemp[:args.brightemp.find(",")]
+	#print("brightness is " + brightness)
+	cct = args.brightemp[args.brightemp.find(",") + 1 :]
+	#print("cct is " + cct)
+	MyBulb.set_brightness_and_color_temperature(int(brightness), int(cct))
+
+	
+
 	
 if args.scene:
 	MyBulb.set_scene(args.scene)
